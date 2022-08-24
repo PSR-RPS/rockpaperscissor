133147cc1d949c7316c2a707247fbae515c08825
PSR-RPS <ragotham@gmail.com>
2022-08-24 01:23:38 -0400

    Add fun to run five rounds of game and compute score

--- gamecontrol.js
+++ gamecontrol.js
@@ -1,23 +1,51 @@
+
+
+
+let computerScore=0;
+let playerScore=0;
+game();
+
 /*getComputerChoice generates and returns random choice selection by computer*/
 function getComputerChoice() {
     const choice = ['rock','paper','scissor'];
     return choice[Math.floor(Math.random()*3)];
 }
-const computerSelection = getComputerChoice();
-console.log(computerSelection);
-const playerSelection = prompt("Enter Player selection rock, paper od scissor");
-console.log(playRound(playerSelection.toLowerCase(),computerSelection));
-
 /*playRound compares player and computer selection to compute win, tie and looser */
 function playRound(playerSelection,computerSelection) {
+    
     if(computerSelection=='rock'& playerSelection=='paper' || computerSelection=='paper'& playerSelection=='scissor' || computerSelection=='scissor'& playerSelection=='rock') {
-        return `You won ${playerSelection} beats ${computerSelection}`;
-        
+        console.log(`This round, You won ${playerSelection} beats ${computerSelection}`);
+        ++playerScore;
     }
     else if(computerSelection === playerSelection) {
-        return `Its a Tie between ${playerSelection} and ${computerSelection}`;
+        console.log(`This round, Its a Tie between ${playerSelection} and ${computerSelection}`);
     }
     else {
-        return `You Lost ${computerSelection} beats ${playerSelection}`;
+        console.log(`This round, You Lost ${computerSelection} beats ${playerSelection}`);
+        ++computerScore;
     }
 }
+
+/*game to run five rounds of game and to compute score and winner at end of 5th round */
+function game() {
+    
+
+    for(i=0;i<5;i++) {
+        const computerSelection = getComputerChoice();
+        //console.log(computerSelection);
+        const playerSelection = prompt("Enter Player selection rock, paper or scissor");
+        playRound(playerSelection.toLowerCase(),computerSelection);
+
+    }
+    if(playerScore>computerScore) {
+        console.log(`Player won with score ${playerScore} vs computerScore ${computerScore}`);
+    }
+    else if(playerScore<computerScore) {
+        console.log(`Player lost with score ${playerScore} vs computerScore ${computerScore}`);
+    }
+    else {
+        console.log(`It's a tie player score ${playerScore} vs computerScore ${computerScore}`);
+
+    }
+}
+
